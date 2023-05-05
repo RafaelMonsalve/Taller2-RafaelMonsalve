376f7c7d592923046c494253efd6ad1452aee115
RafaelMonsalve <mlrh26@hotmail.com>
2023-05-05 10:39:17 -0500

    Se reaizan cambios en el index.html y index.ts

--- src/class/api.ts
+++ src/class/api.ts
@@ -1,20 +1,117 @@
-export class Api {
-    private token: string ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWNhY2lvbiI6MTAzMjQ1NDUzMSwiY29ycmVvIjoibWxyaDI2QGhvdG1haWwuY29tIiwiaWF0IjoxNjgxODI1NjY0LCJleHAiOjE2ODI0MzA0NjR9.41E8OhSlDagldUpUqzMgN5Ykp3czXqnHQlbHxOijw3s";
-    static getAll() {
-        return new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.open('GET', 'https://apiestudiantes.maosystems.dev/api', true);
-            request.onload = () => {
-                if (request.status >= 200 && request.status < 400) {
-                    resolve(JSON.parse(request.responseText));
-                    } else {
-                        reject(request.responseText);
-                        }
-                        };
-                        request.onerror = () => {
-                            reject(request.responseText);
-                            };
-                            request.send();
-                            });
-                            }
+interface FormData {
+  tipoIdentificacion: number;
+  numeroIdentificacion: number;
+  nombres:string;
+  apellidos:string;
+  celular:number;
+  correo:string;
+  linkedin:string;
+  github: string;
 }
+
+interface ApiResponse {
+  success: boolean;
+  data: FormData[];
+}
+
+const fetchFormData = async () => {
+  const response = await fetch('https://apiestudiantes.maosystems.dev/estudiantes', {
+    headers: {
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWNhY2lvbiI6MTAzMjQ1NDUzMSwiY29ycmVvIjoibWxyaDI2QGhvdG1haWwuY29tIiwiaWF0IjoxNjgxODI1NjY0LCJleHAiOjE2ODI0MzA0NjR9.41E8OhSlDagldUpUqzMgN5Ykp3czXqnHQlbHxOijw3s'
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error('Ha ocurrido un error al obtener los datos.');
+  }
+
+  const data: ApiResponse = await response.json();
+
+  if (!data.success) {
+    throw new Error('El API ha respondido con un error.');
+  }
+
+  return data.data;
+};
+
+const renderFormData = (data: FormData[]) => {
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
+
+  // Crea la cabecera de la tabla
+  const headerRow = document.createElement('tr');
+  const TipoDocumentoHeader = document.createElement('th');
+  const NumeroDocumentoHeader = document.createElement('th');
+  const NombresHeader = document.createElement('th');
+  const ApellidosHeader = document.createElement('th');
+  const TelefonoHeader = document.createElement('th');
+  const CorreoHeader = document.createElement('th');
+  const LinkedinHeader = document.createElement('th');
+  const GithubHeader = document.createElement('th');
+  TipoDocumentoHeader.textContent = 'tipoIdentificacion';
+  NumeroDocumentoHeader.textContent = 'numeroIdentificacion';
+  NombresHeader.textContent = 'nombres';
+  ApellidosHeader.textContent = 'apellidos';
+  TelefonoHeader.textContent = 'celular';
+  CorreoHeader.textContent = 'correo';
+  LinkedinHeader.textContent = 'linkedin';
+  GithubHeader.textContent = 'github';
+  headerRow.appendChild(TipoDocumentoHeader);
+  headerRow.appendChild(NumeroDocumentoHeader);
+  headerRow.appendChild(NombresHeader);
+  headerRow.appendChild(ApellidosHeader);
+  headerRow.appendChild(TelefonoHeader);
+  headerRow.appendChild(CorreoHeader);
+  headerRow.appendChild(LinkedinHeader);
+  headerRow.appendChild(GithubHeader);
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+
+  // Crea las filas de la tabla con los datos del formulario
+  data.forEach(formData => {
+    const row = document.createElement('tr');
+    const TipoDocumentoCell = document.createElement('td');
+    const NumeroDocumentoCell = document.createElement('td');
+    const nombresCell = document.createElement('td');
+    const apellidosCell = document.createElement('td');
+    const celularCell = document.createElement('td');
+    const correoCell = document.createElement('td');
+    const linkedinCell = document.createElement('td');
+    const githubCell = document.createElement('td');
+    TipoDocumentoCell.textContent = formData.tipoIdentificacion.toString();
+    NumeroDocumentoCell.textContent = formData.numeroIdentificacion.toString();
+    nombresCell.textContent = formData.nombres;
+    apellidosCell.textContent = formData.apellidos;
+    celularCell.textContent = formData.celular.toString();
+    correoCell.textContent = formData.correo;
+    linkedinCell.textContent = formData.linkedin;
+    githubCell.textContent = formData.github;
+    row.appendChild(TipoDocumentoCell);
+    row.appendChild(NumeroDocumentoCell);
+    row.appendChild(nombresCell);
+    row.appendChild(apellidosCell);
+    row.appendChild(celularCell);
+    row.appendChild(correoCell);
+    row.appendChild(linkedinCell);
+    row.appendChild(githubCell);
+    tbody.appendChild(row);
+  });
+
+  table.appendChild(tbody);
+
+  // Agrega la tabla al documento HTML
+  const container = document.getElementById('Tabla');
+  container!.appendChild(table);
+};
+
+const showFormData = async () => {
+  try {
+    const formData = await fetchFormData();
+    renderFormData(formData);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+showFormData();
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,34 @@
-//Importa la clase persona
-import {Persona} from "./class/Persona.js";
-//Se crea una interfas para poder editar cada dato más adelante
-interface persona{
-   tipodocumento:string;
-   numerodocumento:number;
-   nombres:string;
-   apellidos:string;
-   telefono:number;
-   correo:string;
-   linkedin:string;
-   github:string;
-}
-function PersonaNew(){
-    const tipodocumento = (document.getElementById("tipoDocumento") as HTMLInputElement).value;
-    const numerodocumento= (document.getElementById("numerodocumento")as HTMLInputElement).valueAsNumber;
-    const nombres = (document.getElementById("Nombres")as HTMLInputElement).value;
-    const apellidos = (document.getElementById("Apellidos")as HTMLInputElement).value;
-    const telefono = (document.getElementById("Telefono")as HTMLInputElement).valueAsNumber;
-    const correo = (document.getElementById("correo")as HTMLInputElement).value;
-    const linkedin = (document.getElementById("linkedin")as HTMLInputElement).value;
-    const github = (document.getElementById("github")as HTMLInputElement).value;
-//Validación de datos
-    if(!tipodocumento ||
-        !numerodocumento ||
-        !nombres ||
-        !apellidos ||
-        !telefono ||
-        !correo ||
-        !linkedin ||
-        !github){
-        alert("Debe ingresar todos los datos");
-        return;
-        }
-        //detona el envío de la información a la clase
-        else{    
-    let NewPersona = new Persona (
-        tipodocumento,
-        numerodocumento,
-        nombres,
-        apellidos,
-        telefono,
-        correo,
-        linkedin, 
-        github);NewPersona.persona();
-    }
-}
+ // Evita que el formulario se envíe de forma automática 
+const submitForm = async(event:Event) => {
+    event.preventDefault();
+ // Obtiene los valores de los campos del formulario 
+    const { form } = event.target as HTMLFormElement;
 
+    const  TipoDocumento = form.tipoDocumento.value;
+    const  NumeroDocumento = form.numerodocumento.value;
+    const  Nombres = form.Nombres.value;
+    const  Apellidos= form.Apellidos.value;
+    const  Telefono= form.Telefono.value;
+    const  Correo= form.Correo.value;
+    const  Linkedin= form.linkedin.value;
+    const  Github= form.Github.vale;
+// Envía los datos del formulario al API con token
+    const response = await fetch('https://apiestudiantes.maosystems.dev/estudiantes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWNhY2lvbiI6MTAzMjQ1NDUzMSwiY29ycmVvIjoibWxyaDI2QGhvdG1haWwuY29tIiwiaWF0IjoxNjgxODI1NjY0LCJleHAiOjE2ODI0MzA0NjR9.41E8OhSlDagldUpUqzMgN5Ykp3czXqnHQlbHxOijw3s'
+    },
+    body: JSON.stringify({ TipoDocumento, NumeroDocumento, Nombres, Apellidos, Telefono, Correo, Linkedin, Github })
+  });
+  if (response.ok) {
+    // Si la respuesta del API es exitosa, muestra un mensaje de confirmación
+    alert('¡Usuario registrado!');
+  } else {
+    // Si la respuesta del API no es exitosa, muestra un mensaje de error
+    alert('No fue poible crear el registro');
+  }
+};
+// Agrega un evento "submit" al formulario para procesarlo cuando se envíe
+const form = document.getElementById('formluario');
+form!.addEventListener('submit', submitForm);
